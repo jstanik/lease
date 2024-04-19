3ea59142fb65bb29635f5e5107a78c7b644a1d5e
Juraj Staník <juraj.stanik@gmail.com>
2024-04-19 18:11:54 +0200

    Add unit test for the Transaction class.

--- lease-storage-postgresql/build.gradle.kts
+++ lease-storage-postgresql/build.gradle.kts
@@ -13,12 +13,14 @@ dependencies {
     implementation(project(":lease-api"))
     implementation(libs.jdbc.driver.postgresql)
 
+    testImplementation(libs.google.truth)
+    testImplementation(libs.hikari.cp)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.mockito.junit.jupiter)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.testcontainers.postgresql)
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation(libs.junit.jupiter)
-    testImplementation(libs.testcontainers.postgresql)
-    testImplementation(libs.hikari.cp)
-    testImplementation(libs.google.truth)
 }
 
 tasks.test {
--- lease-storage-postgresql/src/test/kotlin/sk/bakeit/lease/storage/postgresql/jdbc/TransactionTest.kt
+++ lease-storage-postgresql/src/test/kotlin/sk/bakeit/lease/storage/postgresql/jdbc/TransactionTest.kt
@@ -1,5 +1,63 @@
 package sk.bakeit.lease.storage.postgresql.jdbc
 
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.sql.Connection
+
+@ExtendWith(MockitoExtension::class)
 class TransactionTest {
-    // TODO
+
+    // These tests incur a memory leak due to not cleaning up a ThreadLocal variable
+    // inside Transaction class. Introducing cleanup code would decrease the readability
+    // of the code and hide the test purpose.
+
+    @Mock
+    lateinit var connection: Connection
+
+    @Test
+    fun `getCurrentTransaction called twice from the same thread returns same instance`() {
+        val actual1 = Transaction.getCurrentTransaction { connection }
+        val actual2 = Transaction.getCurrentTransaction { connection }
+
+        assertThat(actual2).isSameInstanceAs(actual1)
+    }
+
+    @Test
+    fun beginTransaction() {
+        val cut = Transaction.getCurrentTransaction { connection }
+
+        whenever(connection.autoCommit).thenReturn(true)
+        whenever(connection.transactionIsolation).thenReturn(Connection.TRANSACTION_NONE)
+
+        cut.beginTransaction()
+        cut.beginTransaction() // reentrant - should have no effect
+
+        verify(connection, times(1)).autoCommit = false
+        verify(connection, times(1)).transactionIsolation = Connection.TRANSACTION_READ_COMMITTED
+    }
+
+    @Test
+    fun endTransaction() {
+        val cut = Transaction.getCurrentTransaction { connection }
+        whenever(connection.autoCommit).thenReturn(true)
+        whenever(connection.transactionIsolation).thenReturn(Connection.TRANSACTION_NONE)
+        cut.beginTransaction()
+        verify(connection, times(1)).autoCommit = false
+        verify(connection, times(1)).transactionIsolation = Connection.TRANSACTION_READ_COMMITTED
+        
+        cut.endTransaction()
+
+        verify(connection, times(1)).autoCommit = true
+        verify(connection, times(1)).transactionIsolation = Connection.TRANSACTION_NONE
+        val otherTransaction = Transaction.getCurrentTransaction { connection }
+        assertThat(otherTransaction).isNotSameInstanceAs(cut)
+    }
+
+
 }
